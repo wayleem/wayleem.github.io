f4fd74a608f1a34ef098382348eb8e02d25fbf09
way_leem <wayleemh@gmail.com>
2022-12-06 06:28:02 -0500

    amend(sidebar): position icons.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,25 +4,26 @@ import LogoSubtitle from "../assets/images/sublogo.png"
 
 const Sidebar = () => {
     return (
-        <div className="top-0 sm:h-screen sm:w-16 bg-zinc-900 text-[#4d4d4e] h-16 w-screen">
-            <ul className="sm:sidebar-col sidebar-col-mobile sm:h-16">
+        <div className="bg-zinc-900 text-[#4d4d4e] top-0 left-0 h-16 w-screen sidebar-col-mobile sm:h-screen sm:w-16 sm:sidebar-col fixed">
 
-                <Link className="sm:w-14 w-12" to="/">
-                    <img className="" src={LogoW} />
-                    <img className="" src={LogoSubtitle} />
-                </Link>
+            <Link className="w-12 mx-2 sm:m-4" to="/">
+                <img src={LogoW} />
+                <img className="sm:flex hidden" src={LogoSubtitle} />
+            </Link>
 
-                <NavLink to="/" className="fa-solid fa-home p-4 text-2xl" />
-                <NavLink to="/about" className="fa-solid fa-user p-4 text-2xl" />
-                <NavLink to="/contact" className="fa-solid fa-envelope p-4 text-2xl" />
+            <nav className="text-2xl my-1 sm:sidebar-col sm:relative sm:mt-auto">
+                <NavLink to="/" className="fa-solid fa-home p-4" />
+                <NavLink to="/about" className="fa-solid fa-user p-4" />
+                <NavLink to="/contact" className="fa-solid fa-envelope p-4" />
+            </nav>
 
+            <div className="hidden sm:sidebar-col sm:relative sm:mt-auto">
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/will-huang2/" className="fa-brands fa-linkedin sidebar-external-link" />
+                <a target="_blank" rel="noreferrer" href="https://github.com/Wayleem/" className="fa-brands fa-github sidebar-external-link" />
+                <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/203924694192226305/" className="fa-brands fa-discord sidebar-external-link" />
+            </div>
 
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/will-huang2/" className="fa-brands fa-linkedin p-1 text-sm" />
-                <a target="_blank" rel="noreferrer" href="https://github.com/Wayleem/" className="fa-brands fa-github p-1 text-sm" />
-                <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/203924694192226305/" className="fa-brands fa-discord p-1 text-sm" />
-
-            </ul>
-        </div >
+        </div>
     )
 }
 
